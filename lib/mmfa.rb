e4b7ea37b9171fe97fe461d70fc78105f3c6b358
Tommy Bishop <bishop.thomas@gmail.com>
2013-05-01 18:30:46 -0700

    Handle a missing keychain item.
    Also, I just couldn't resist "fixing" your perl code. ;)

--- lib/mmfa.rb
+++ lib/mmfa.rb
@@ -4,10 +4,17 @@ require "google_authenticator_auth"
 
 module Mmfa
   def self.current_key(keychain_item_name)
-    secret = `security find-generic-password -ga #{keychain_item_name} 2>&1 | perl -e 'if (<STDIN> =~ m/password: "(.*)"$/ ) { print $1; }'`
-    ga = GoogleAuthenticator.new(secret)
-    current_key = ga.get_keys[1]
-    `printf '#{current_key}' | pbcopy`
-    puts current_key
+    secret_output = `security find-generic-password -ga #{keychain_item_name} 2>&1`
+
+    /password: "(?<password>.*)"$/.match(secret_output) do |m|
+      ga = GoogleAuthenticator.new(m['password'])
+      current_key = ga.get_keys[1]
+      `printf '#{current_key}' | pbcopy`
+      puts current_key
+      exit
+    end
+
+    puts "Unable to get the password for '#{keychain_item_name}'"
+    exit 1
   end
 end
